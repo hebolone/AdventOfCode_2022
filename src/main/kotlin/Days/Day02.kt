ab65cbe5d9da7e116178d85fa3a72f1c36c207d7
Hebolone <polo.simone@gmail.com>
2022-12-03 11:38:53 +0100

    Day 2 resolved + code refine

--- src/main/kotlin/Days/Day02.kt
+++ src/main/kotlin/Days/Day02.kt
@@ -5,17 +5,17 @@ class Day02 : DayBase(), ISolver {
 
     override fun advanced() : Any = _pointsAdvanced
 
-    private data class Move(val move : String, val pointsForMyMove : Int, val pointsForPlay : Int, val pointsForMyMoveAdvanced : Int, val pointsForPlayAdvanced : Int, val routing : String)
+    private data class Move(val move : String, val pointsForMyMove : Int, val pointsForPlay : Int, val routing : String)
     private val _moves = listOf(
-        Move("A X", 1, 3, 3, 0, "Z"),
-        Move("A Y", 2, 6, 1, 3, "X"),
-        Move("A Z", 3, 0, 2, 6, "Y"),
-        Move("B X", 1, 0, 1, 0, "X"),
-        Move("B Y", 2, 3, 2, 3, "Y"),
-        Move("B Z", 3, 6, 3, 6, "Z"),
-        Move("C X", 1, 6, 2, 0, "Y"),
-        Move("C Y", 2, 0, 3, 3, "Z"),
-        Move("C Z", 3, 3, 1, 6, "X")
+        Move("A X", 1, 3, "Z"),
+        Move("A Y", 2, 6, "X"),
+        Move("A Z", 3, 0, "Y"),
+        Move("B X", 1, 0, "X"),
+        Move("B Y", 2, 3, "Y"),
+        Move("B Z", 3, 6, "Z"),
+        Move("C X", 1, 6, "Y"),
+        Move("C Y", 2, 0, "Z"),
+        Move("C Z", 3, 3, "X")
     )
     private var _points = 0
     private var _pointsAdvanced = 0
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,6 @@ fun main() {
     DaysBuilder("/home/simone/Scrivania/AdventOfCode/2022")             //    Set input path
         //.addDay(1, Day01())
         .addDay(2, Day02())
-        //.setTest(1)
+        //.setTest(2)
         .solve(2)                //    Default is 'both'
 }
